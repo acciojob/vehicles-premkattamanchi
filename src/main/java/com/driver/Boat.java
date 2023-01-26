5be57fe007b7b19c9a3d10fd301365f4fa0336be
prem <premkattamanchi@gmail.com>
2023-01-26 22:29:07 +0530

    modified twice

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,19 @@ public class Boat implements WaterVehicle {
 
     private String name;
     private int capacity;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Boat() {
+
+    }
+
     @Override
     public String getVehicleName() {
         return this.name;
